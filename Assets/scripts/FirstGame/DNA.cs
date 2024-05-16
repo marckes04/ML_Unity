0d982ff75d76a7035a06051e82c6a7404690d850
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2024-05-16 13:01:39 -0500

    DNA Excercise completed

---
+++ Assets/scripts/FirstGame/DNA.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNA : MonoBehaviour
+{
+    // gene for colour
+    public float r;
+    public float g;
+    public float b;
+    bool dead = false;
+    public float timeToDie = 0;
+    SpriteRenderer sRenderer;
+    Collider2D sCollider;
+
+     void OnMouseDown()
+    {
+        dead = true;
+        timeToDie = PopulationManager.elapsed;
+        //Debug.Log("Dead At: " + timeToDie);
+        sRenderer.enabled = false;
+        sCollider.enabled = false;
+    }
+
+    void Start()
+    {
+       sRenderer = GetComponent<SpriteRenderer>();
+       sCollider = GetComponent<Collider2D>();
+       sRenderer.color = new Color(r,g, b);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
